b2b4380faace6e6aa42a7cf271a6cb5bc38c69d1
Richelle Acheampong <richelleach7@gmail.com>
2026-08-05 15:16:46 +0100

    Create homepage with upload and search interface

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    file = request.files.get("notes")
+
+    if file:
+        return f"Uploaded: {file.filename}"
+
+    return "No file uploaded."
 
 
 if __name__ == "__main__":
